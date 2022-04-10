57e9f4b61a9a569f1503b01c0836b3abbd28f8ae
Paul Kenneth Egell-Johnsen <paul.k.egell.johnsen@gmail.com>
2022-04-10 16:24:46 +0200

    Revert "adding simple keyboard component"
    This reverts commit 9f2649c604ff305b0cb845d2ec72888fba90163b.

--- pages/game.vue
+++ pages/game.vue
@@ -1,18 +1,5 @@
 <script setup lang="ts">
-import SimpleKeyboard from '../components/SimpleKeyboard.vue';
 const { state, submitGuess, resetGame } = useGameState()
-const input = ref('')
-
-function onChange(inputChange) {
-  input.value = inputChange
-  console.log("Input changed", input)
-}
-function onKeyPress(button) {
-  console.log("button", button)
-}
-function onInputChange(inputChange) {
-  input.value = inputChange.target.value
-}
 </script>
 
 <template>
@@ -30,7 +17,6 @@ function onInputChange(inputChange) {
     <section>
       <GameBoard :state="state" />
       <GuessForm @guess="submitGuess" />
-      <SimpleKeyboard :input="input" :keyboardClass="keyboardClass" />
       <button class="secondary outline" @click="resetGame">Reset game</button>
     </section>
   </div>
